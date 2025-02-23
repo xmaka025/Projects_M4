284280caecb81ce13d900d36c252789e68f24393
Antonie <antonina20031611@gmail.com>
2025-02-23 13:41:29 +0100

    ok

---
+++ Cviceni2.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
+
+
+def valid_bracket(s: str) -> bool:
+    bracket = {')': '(', '}': '{', ']': '['}
+    stack = Stack()
+
+    for open_br in s:
+        if open_br in '({[':
+            stack.push(open_br)
+        elif open_br in ')}]':
+            ls_open = stack.pop()
+            if ls_open != bracket[open_br]:
+                return False
+
+    return stack.is_empty()
+
+
+if __name__ == "__main__":
+    test = ["([]{})", "([)]", "{[}]", "()"]
+
+    for test in test:
+        result = "Правильная" if valid_bracket(test) else "Неправильная"
+        print(f"Строка '{test}': {result}")
