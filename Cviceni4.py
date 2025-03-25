c53fac63ab2edcf87a34a37fce14a175452b3788
Antonie <antonina20031611@gmail.com>
2025-03-25 15:38:59 +0100

    ok

---
+++ Cviceni4.py
@@ -0,0 +1,30 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Queue is empty")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+print("Размер очереди:", queue.size())  # Размер очереди: 3
+
+while not queue.is_empty():
+    item = queue.dequeue()
+    print("Извлечен элемент:", item)
