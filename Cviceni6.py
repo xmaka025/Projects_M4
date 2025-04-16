312fc29827f5eada9529a54b157ba2505541c025
Antonie <antonina20031611@gmail.com>
2025-04-16 23:04:05 +0200

    ok

---
+++ Cviceni6.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({self.value})'
+
+
+class Graph:
+    def __init__(self, root):
+        self._root = root
+
+    def dfs(self):
+        def dfs1(node, visitor):
+            visitor.add(node)
+            print(node.value)
+
+            for sosed in node.outbound:
+                if sosed not in visitor:
+                    dfs1(sosed, visitor)
+
+        visited = set()
+        dfs1(self._root, visited)
+        return [node.value for node in visited]
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+
+g = Graph(a)
+
+print(g.dfs())
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+b.point_to(f)
+c.point_to(e)
+
+g = Graph(a)
+
+print(g.dfs())
