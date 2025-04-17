7fea31e963fbcc330665bc6291a92d2f3179fe71
Antonie <antonina20031611@gmail.com>
2025-04-17 16:05:43 +0200

    ok

--- Cviceni8.py
+++ Cviceni8.py
@@ -30,3 +30,21 @@ def insertion_sort(arr):
 my_list = [64, 34, 25, 12, 22, 11, 90, 0]
 insertion_sort(my_list)
 print("Отсортированный список:", my_list)
+
+
+#selection_sort
+
+
+def selection_sort(arr):
+    for step in range(len(arr)):
+        min_index = step
+        for i in range(step + 1, len(arr)):
+            if arr[i] < arr[min_index]:
+                min_index = i
+
+        (arr[step], arr[min_index]) = (arr[min_index], arr[step])
+
+
+my_list = [64, 34, 25, 12, 22, 11, 90, 0]
+selection_sort(my_list)
+print("Отсортированный список:", my_list)
