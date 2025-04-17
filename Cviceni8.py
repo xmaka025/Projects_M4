b2c439388c4cec84285170e60192d595c9b75c14
Antonie <antonina20031611@gmail.com>
2025-04-17 15:56:35 +0200

    ok

--- Cviceni8.py
+++ Cviceni8.py
@@ -1,3 +1,5 @@
+#bubble_sort
+
 def bubble_sort(arr):
     for i in range(len(arr)):
         for j in range(0, len(arr) - i - 1):
@@ -9,4 +11,22 @@ def bubble_sort(arr):
 
 my_list = [64, 34, 25, 12, 22, 11, 90, 0]
 bubble_sort(my_list)
+print("Отсортированный список:", my_list)
+
+
+#insertion_sort
+
+def insertion_sort(arr):
+    for step in range(1, len(arr)):
+        key = arr[step]
+        j = step - 1
+
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = key
+
+
+my_list = [64, 34, 25, 12, 22, 11, 90, 0]
+insertion_sort(my_list)
 print("Отсортированный список:", my_list)
